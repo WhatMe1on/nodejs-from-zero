78805e91571c7cb4d30064346666879a079a97f7
Whatmelon <ccacrop.code@gmail.com>
2025-02-15 20:32:55 +0800

    默认提交 - Default submission

--- geektime/async/index.js
+++ geektime/async/index.js
@@ -1,9 +1,20 @@
-console.log(async function () {
-    throw new Error('4')
-}())
+(function () {
+    const result = async function () {
+        try {
+            var content = await new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    reject(new Error('8'));
+                }, 500)
+            })
+        } catch (e) {
+            console.log('error', e.message);
+        }
 
-console.log(function () {
-    return new Promise((resolve, reject) => {
-        reject(new Error('4'));
-    })
-}())
+        console.log(content)
+        return 4
+    }()
+
+    setTimeout(() => {
+        console.log(result)
+    }, 800)
+})()
