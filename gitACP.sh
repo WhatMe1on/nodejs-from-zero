df199ec749d78c471e670d83491709a29765ef49
Whatmelon <ccacrop.code@gmail.com>
2025-02-15 16:23:50 +0800

    默认提交 - Default submission

--- gitACP.sh
+++ gitACP.sh
@@ -3,19 +3,25 @@
 # 设置默认的 commit message
 DEFAULT_MSG="默认提交"
 
-# 判断第一个参数
-if [ -z "$1" ]; then
-    # 如果没有提供参数，则执行 git add、commit 和 push
-    COMMIT_MSG="$DEFAULT_MSG"
-    git add . 
+# 解析第一个参数
+ACTION="$1"
+shift # 移除第一个参数，剩下的作为 commit message
+
+# 使用传入的 commit message，否则使用默认值
+COMMIT_MSG="${1:-$DEFAULT_MSG}"
+
+# 翻译 commit message
+english_text=$(trans -b :en "$COMMIT_MSG")
+
+# 拼接中文和英文 commit message
+COMMIT_MSG="$COMMIT_MSG - $english_text"
+
+if [ "$ACTION" == "commit" ]; then
     git commit -m "$COMMIT_MSG"
-    git push
-elif [ "$1" = "commit" ]; then
-    # 如果第一个参数是 commit，则只执行 git commit
-    COMMIT_MSG="${2:-$DEFAULT_MSG}"
-    git commit -m "$COMMIT_MSG"
+elif [ -z "$ACTION" ]; then
+    git add . && git commit -m "$COMMIT_MSG" && git push
 else
-    echo "无效参数: $1"
-    echo "用法: $0 [commit] [commit_message]"
-    exit 1
+    echo "无效的参数，使用方式："
+    echo "  $0            # 执行 git add、commit、push"
+    echo "  $0 commit     # 仅执行 git commit"
 fi
